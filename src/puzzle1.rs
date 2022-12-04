aaa5a84423c3393052bd7b6f899db34ec100e022
Delu <fjaviercamacholopez@gmail.com>
2022-12-04 19:17:12 +0100

    Problem 1 solved + Started Problem 2

---
+++ src/puzzle1.rs
@@ -0,0 +1,53 @@
+﻿use std::fs;
+
+struct Elf {
+    calories: i32,
+}
+
+impl Elf {
+    fn new() -> Elf {
+        Elf { calories: 0 }
+    }
+
+    pub fn add_calories(&mut self, calories: i32) {
+        self.calories += calories;
+    }
+
+    pub fn get_calories(&self) -> i32 {
+        self.calories
+    }
+}
+
+pub fn resolve() {
+    let path_file = "input/puzzle1.txt";
+
+    let file_content = fs::read_to_string(path_file)
+        .expect("Something went wrong reading the file");
+
+
+    let mut elves = Vec::new();
+    let elf = Elf::new();
+    elves.push(elf);
+
+    let mut index = 0;
+
+    for line in file_content.lines() {
+        if str::is_empty(line) {
+            let elf = Elf::new();
+            elves.push(elf);
+            index += 1;
+        } else {
+            let calories = line.parse::<i32>().unwrap();
+            elves[index].add_calories(calories);
+        }
+    }
+
+    //Finding the elf with highest calories.
+    let max_calories = elves.iter().map(Elf::get_calories).max().unwrap();
+
+    //Finding the three with more calories
+    elves.sort_by(|a, b| b.get_calories().cmp(&a.get_calories()));
+
+    println!("{}", max_calories);
+    println!("{}", elves[0].get_calories() + elves[1].get_calories() + elves[2].get_calories());
+}
---
+++ src/puzzle2.rs
@@ -0,0 +1,75 @@
+﻿use std::fs;
+
+enum EnemyPlay {
+    A, //Rock
+    B, //Paper
+    C, //Scissor
+}
+
+enum PlayerPlay {
+    X, //Rock
+    Y, //Paper
+    Z, //Scissor
+}
+
+struct Strategy {
+    enemy_play: EnemyPlay,
+    player_play: PlayerPlay,
+}
+
+impl Strategy {
+    pub fn calculate_score(&self) -> i32 {
+        let mut score = 0;
+        
+        let player_play_score = match self.player_play {
+            PlayerPlay::X => {1}
+            PlayerPlay::Y => {2}
+            PlayerPlay::Z => {3}
+        };
+        
+        score += player_play_score;
+        
+        let battle_score = match self.player_play {
+            PlayerPlay::X => {
+                let versus_enemy_score = match self.enemy_play {
+                    EnemyPlay::A => {1}
+                    EnemyPlay::B => {0}
+                    EnemyPlay::C => {2}
+                };
+                
+                versus_enemy_score
+            }
+            PlayerPlay::Y => {
+                let versus_enemy_score = match self.enemy_play {
+                    EnemyPlay::A => {2}
+                    EnemyPlay::B => {1}
+                    EnemyPlay::C => {0}
+                };
+
+                versus_enemy_score
+            }
+            PlayerPlay::Z => {
+                let versus_enemy_score = match self.enemy_play {
+                    EnemyPlay::A => {0}
+                    EnemyPlay::B => {2}
+                    EnemyPlay::C => {1}
+                };
+
+                versus_enemy_score
+            }
+        };
+        
+        score += battle_score;
+        
+        score
+    }
+}
+
+pub fn resolve() {
+    let path_file = "input/puzzle2.txt";
+
+    let file_content = fs::read_to_string(path_file)
+        .expect("Something went wrong reading the file");
+    
+    
+}
